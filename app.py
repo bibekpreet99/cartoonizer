32dd5552c0b655bbd15e87e63961a3972537c4a2
Bibekpreet Singh <bibekpreet99@gmail.com>
2021-10-15 17:00:58 +0530

    heroku

--- app.py
+++ app.py
@@ -38,9 +38,16 @@ def cartoonize(img, path):
     cv2.imwrite("./static/images/test.png", cartoon_image)
 
 
-@app.route('/')
+@app.route('/', method = ["GET", "POST"])
 def index():
-    # if request.method == "GET":
+    if request.method == "POST":
+         if request.files["file"]:
+                
+            f = request.files["file"]
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            cartoonize(os.path.join(app.config["UPLOAD_FOLDER"], filename), os.path.join(app.config["UPLOAD_FOLDER"]))           
+            return send_file(os.path.join(app.config["UPLOAD_FOLDER"], "test.png"), as_attachment = True)
     #     if not os.path.exists(app.config["UPLOAD_FOLDER"]):
     #         os.mkdir(app.config["UPLOAD_FOLDER"])
     #     for img in os.listdir(app.config["UPLOAD_FOLDER"]):
@@ -48,16 +55,16 @@ def index():
     #     return render_template("home.html")
     return render_template("index.html")
 
-@app.route('/file', methods = ["POST"])
-def file():
-    if request.method == "POST":
-        print(request.files["file"])
-        if request.files["file"]:
+# @app.route('/file', methods = ["POST"])
+# def file():
+#     if request.method == "POST":
+#         print(request.files["file"])
+#         if request.files["file"]:
             
-            f = request.files["file"]
-            filename = secure_filename(f.filename)
-            f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-            cartoonize(os.path.join(app.config["UPLOAD_FOLDER"], filename), os.path.join(app.config["UPLOAD_FOLDER"]))           
-            return send_file(os.path.join(app.config["UPLOAD_FOLDER"], "test.png"), as_attachment = True)
-    return redirect(url_for('index'))
+#             f = request.files["file"]
+#             filename = secure_filename(f.filename)
+#             f.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+#             cartoonize(os.path.join(app.config["UPLOAD_FOLDER"], filename), os.path.join(app.config["UPLOAD_FOLDER"]))           
+#             return send_file(os.path.join(app.config["UPLOAD_FOLDER"], "test.png"), as_attachment = True)
+#     return redirect(url_for('index'))
 
